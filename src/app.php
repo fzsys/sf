c5a00a2a804da0e6e848d544a69b6594fbd80953
igor uhan <igoruhan@nxte.nl>
2022-01-19 10:14:34 +0200

    added controllers support (incomplete)

--- src/app.php
+++ src/app.php
@@ -1,12 +1,31 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
+
+function is_leap_year(int $year = null): bool {
+  if (null === $year) {
+    $year = date('Y');
+  }
+
+  return $year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0);
+}
 
 $routes = new RouteCollection();
 
 $routes->add('home', new Route('/'));
 $routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
 $routes->add('bye', new Route('/bye'));
+$routes->add('leap_year', new Route('/is-leap-year/{year}', [
+  'year' => NULL,
+  '_controller' => function(Request $request) {
+    if (is_leap_year($request->attributes->get('year'))) {
+      return new Response('This is a leap year!');
+    }
+    return new Response('This is not leap year.');
+  },
+]));
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -1,12 +1,20 @@
 <?php
 
+require_once __DIR__ . ('/../vendor/autoload.php');
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
-require_once __DIR__ . ('/../vendor/autoload.php');
+function render_template(Request $request): Response {
+  extract($request->attributes->all(), EXTR_SKIP);
+  ob_start();
+  include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+  return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
@@ -16,10 +24,8 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-  extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-  ob_start();
-  include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-  $response = new Response(ob_get_clean());
+  $request->attributes->add($matcher->match($request->getPathInfo()));
+  $response = call_user_func('render_template', $request);
 } catch (ResourceNotFoundException $exception) {
   $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
