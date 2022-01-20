2d7e9337a3ec010dc6343f8d0ae616f7c92a2196
igor uhan <igoruhan@nxte.nl>
2022-01-20 09:44:54 +0200

    added support of http-kernel

--- src/app.php
+++ src/app.php
@@ -5,14 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-function is_leap_year(int $year = null): bool {
-  if (null === $year) {
-    $year = date('Y');
-  }
-
-  return $year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0);
-}
-
 $routes = new RouteCollection();
 
 $routes->add('home', new Route('/'));
@@ -20,12 +12,27 @@ $routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
 $routes->add('bye', new Route('/bye'));
 $routes->add('leap_year', new Route('/is-leap-year/{year}', [
   'year' => NULL,
-  '_controller' => function(Request $request) {
-    if (is_leap_year($request->attributes->get('year'))) {
-      return new Response('This is a leap year!');
-    }
-    return new Response('This is not leap year.');
-  },
+  '_controller' => 'LeapYearController::index'
 ]));
 
 return $routes;
+
+
+class LeapYearController {
+
+  public function index($year): Response {
+    if ($this->is_leap_year($year)) {
+      return new Response('This is a leap year!');
+    }
+    return new Response('This is not leap year.');
+  }
+
+  private function is_leap_year(int $year = null): bool {
+    if (null === $year) {
+      $year = date('Y');
+    }
+
+    return $year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0);
+  }
+
+}
