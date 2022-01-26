0dcd75e01545415d732cac226b2fa5073fa1916f
igor uhan <igoruhan@nxte.nl>
2022-01-26 09:45:26 +0200

    added DI support

--- web/index.php
+++ web/index.php
@@ -2,43 +2,12 @@
 
 require_once __DIR__ . ('/../vendor/autoload.php');
 
-use App\Core;
-use App\StringResponseListener;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\EventListener\StreamedResponseListener;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
+
+/** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
+$container = include __DIR__ . '/../src/container.php';
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-$routes = include __DIR__ . '/../src/app.php';
-
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$listener = new ErrorListener(
-  '\Calendar\Controller\ErrorController::exception'
-);
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber($listener);
-$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new StreamedResponseListener());
-$dispatcher->addSubscriber(new StringResponseListener());
-
-$core = new Core($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$response = $core->handle($request);
+$response = $container->get('core')->handle($request);
 
 $response->send();
